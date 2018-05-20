e9cce63bd9b1099e0a0e50635d4bdb087983bd66
d9e7381f <939770681@qq.com>
2018-05-20 15:38:55 +0800

    comment board

---
+++ src/pages/oj/views/problem/MessageBoard.vue
@@ -0,0 +1,30 @@
+<template>
+  <Panel shadow :padding="10">
+    <div slot="title">
+      留言板
+    </div>
+    
+  </Panel>
+</template>
+<script>
+export default {
+  name: 'CommentBoard',
+  data () {
+    return {
+      total: 0,
+      limit: 10,
+      problemID: 0,
+      commentList: []
+    }
+  },
+  methods: {
+    init () {
+      this.problemID = this.$route.params.problemID
+    },
+    getComment () {
+      
+    }
+  }
+
+}
+</script>
