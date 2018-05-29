ed2751b79218545f59c80a9528ffa0113e2a695d
d9e7381f <939770681@qq.com>
2018-05-29 21:40:07 +0800

    finsh newcourseCollection

--- src/pages/admin/views/general/NewCourseCollection.vue
+++ src/pages/admin/views/general/NewCourseCollection.vue
@@ -5,48 +5,79 @@
         <el-breadcrumb-item v-for="item of breadcrumb" :key="item.id" ><a @click="goTag(item.id)">{{item.name}}</a></el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <div>
-      <el-select v-model="selectID" placeholder="请选择">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-      <el-button @click="goforward()" round>进入</el-button>
+    <div style="margin-top: 15px;margin-right: 5px;cursor:pointer">
+      <span v-for="item in options" :key="item.id">
+        <el-tag closable @close="deleteTag(item.name, item.id)" size="medium"><a @click="goforward(item.id)">{{item.name}}</a></el-tag>
+        <icon-btn name="编辑" icon="edit" @click.native="goEdit(item.id)" style="margin-left: 2px;margin-right: 10px;"></icon-btn>
+      </span>
+      <el-input
+        class="input-new-tag"
+        v-if="inputVisible"
+        v-model="inputValue"
+        ref="saveTagInput"
+        size="small"
+        @keyup.enter.native="handleInputConfirm"
+        @blur="handleInputConfirm"
+      >
+      </el-input>
+      <el-button v-else class="button-new-tag" size="small" @click="showInput">添加新节点</el-button>
     </div>
   </Panel>
 </template>
 <script>
+import Vue from 'vue'
 import api from '../../api.js'
 export default {
   name: 'course',
   data () {
     return {
-      selectID: '',
+      currentID: 0,
+      isCourse: true,
       courseList: [],
       collectionList: [],
       selectList: [],
-      breadcrumb: [
-        {
-          name: '课程与分类',
-          id: 21
-        }
-      ],
+      breadcrumb: [],
+      inputVisible: false,
+      inputValue: '',
       options: [
         {
-          label: '课程',
-          value: 0
+          name: '课程',
+          id: 0
         },
         {
-          label: '分类',
-          value: -1
+          name: '分类',
+          id: -1
         }
       ]
     }
   },
   methods: {
+    showInput () {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    handleInputConfirm () {
+      console.log('id: ' + this.currentID)
+      let inputValue = this.inputValue
+      if (inputValue) {
+        let params = {name: inputValue}
+        if (this.currentID !== 0 || this.currentID !== -1) {
+          let parent = this.getItemById(this.selectList, this.currentID)
+          params.parent = parent.url
+        }
+        let funcName = this.isCourse === true ? 'addCourse' : 'addCollection'
+        api[funcName](params).then(res => {
+          if (!res.data.error) {
+            console.log('done')
+          }
+        })
+        this.options.push({name: inputValue})
+      }
+      this.inputVisible = false
+      this.inputValue = ''
+    },
     getCollectionList () {
       api.getCollection().then(res => {
         this.collectionList = res.data.data.collection
@@ -64,37 +95,85 @@ export default {
         if (item.id === id) {
           return item
         }
-        this.getItemById(item.children, id)
+        let res = this.getItemById(item.children, id)
+        if (res) {
+          return res
+        }
       }
     },
+    goEdit (id) {
+      this.$prompt('输入节点的新名称', '重命名', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+      }).then(({value}) => {
+        let funcName = this.isCourse === true ? 'renCourse' : 'renCollection'
+        api[funcName](id, {name: value}).then(res => {
+          if (!res.data.error) {
+            this.options[this.options.findIndex(item => item.id === id)].name = value
+          }
+        })
+      })
+    },
     goTag (id) {
-      console.log('go:' + id)
+      this.currentID = id
+      let index = this.breadcrumb.findIndex(item => item.id === id)
+      this.breadcrumb.splice(index + 1, this.breadcrumb.length - index + 1)
+      if (id === 0) {
+        this.options = [
+          {
+            name: '课程',
+            id: 0
+          },
+          {
+            name: '分类',
+            id: -1
+          }
+        ]
+      } else {
+        this.options = this.getItemById(this.selectList, id).children
+      }
+    },
+    deleteTag (name, id) {
+      if (id === 0 || id === -1) {
+        Vue.prototype.$error('该标签无法删除')
+      } else {
+        this.$confirm('确认删除吗？').then(_ => {
+          let funcName = this.isCourse === true ? 'deleteCourse' : 'deleteCollection'
+          api[funcName](id).then(res => {
+            if (!res.data.error) {
+              this.options.splice(this.options.findIndex(item => item.id === id), 1)
+            }
+          })
+        })
+      }
     },
     changeList (list) {
       for (let item of list) {
-        item.label = item.name
-        item.value = item.id
-        delete item.name
-        delete item.id
+        delete item.url
         delete item.problem
         this.changeList(item.children)
       }
     },
-    goforward () {
-      if (this.selectID === 0) {
+    goforward (id) {
+      this.currentID = id
+      if (id === 0) {
         this.selectList = this.courseList
-        console.log(this.selectList)
         this.options = this.selectList
-      } else if (this.selectID === -1) {
+        this.breadcrumb = []
+        this.isCourse = true
+        this.breadcrumb.push({name: '课程', id: 0})
+      } else if (id === -1) {
         this.selectList = this.collectionList
         this.options = this.selectList
-        console.log(this.selectList)
+        this.breadcrumb = []
+        this.isCourse = false
+        this.breadcrumb.push({name: '分类', id: 0})
       } else {
-        let selectItem = this.getItemById(this.selectList, this.value)
+        let selectItem = this.getItemById(this.selectList, id)
+        console.log(selectItem)
         this.options = selectItem.children
-        this.breadcrumb.push({name: selectItem.label, id: selectItem.value})
+        this.breadcrumb.push({name: selectItem.name, id: selectItem.id})
       }
-      console.log('goforward:' + this.selectID)
     }
   },
   mounted () {
@@ -104,3 +183,21 @@ export default {
 
 }
 </script>
+<style lang="less">
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
+</style>
+
