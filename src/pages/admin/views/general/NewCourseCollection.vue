9c0e8f47b193f729394cf2f7a93cbcd536ecccd2
d9e7381f <939770681@qq.com>
2018-05-29 10:16:12 +0800

    update

--- src/pages/admin/views/general/NewCourseCollection.vue
+++ src/pages/admin/views/general/NewCourseCollection.vue
@@ -19,11 +19,15 @@
   </Panel>
 </template>
 <script>
+import api from '../../api.js'
 export default {
   name: 'course',
   data () {
     return {
       selectID: '',
+      courseList: [],
+      collectionList: [],
+      selectList: [],
       breadcrumb: [
         {
           name: '课程与分类',
@@ -37,18 +41,65 @@ export default {
         },
         {
           label: '分类',
-          value: 1
+          value: -1
         }
       ]
     }
   },
   methods: {
+    getCollectionList () {
+      api.getCollection().then(res => {
+        this.collectionList = res.data.data.collection
+        this.changeList(this.collectionList)
+      }).catch(() => {})
+    },
+    getCourseList () {
+      api.getCourse().then(res => {
+        this.courseList = res.data.data.course
+        this.changeList(this.courseList)
+      }).catch(() => {})
+    },
+    getItemById (list, id) {
+      for (let item of list) {
+        if (item.id === id) {
+          return item
+        }
+        this.getItemById(item.children, id)
+      }
+    },
     goTag (id) {
       console.log('go:' + id)
     },
+    changeList (list) {
+      for (let item of list) {
+        item.label = item.name
+        item.value = item.id
+        delete item.name
+        delete item.id
+        delete item.problem
+        this.changeList(item.children)
+      }
+    },
     goforward () {
+      if (this.selectID === 0) {
+        this.selectList = this.courseList
+        console.log(this.selectList)
+        this.options = this.selectList
+      } else if (this.selectID === -1) {
+        this.selectList = this.collectionList
+        this.options = this.selectList
+        console.log(this.selectList)
+      } else {
+        let selectItem = this.getItemById(this.selectList, this.value)
+        this.options = selectItem.children
+        this.breadcrumb.push({name: selectItem.label, id: selectItem.value})
+      }
       console.log('goforward:' + this.selectID)
     }
+  },
+  mounted () {
+    this.getCollectionList()
+    this.getCourseList()
   }
 
 }
